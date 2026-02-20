61097daf500b7d8c991ce19ca99dd36c4c8e5b05
Hannah <hannahmneal@gmail.com>
2026-02-20 00:30:48 -0600

    Fixed styles and returned .env variables to NSS values

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/postcss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   base: '/', // This is important for assets to load correctly
   server: {
     host: '0.0.0.0',
